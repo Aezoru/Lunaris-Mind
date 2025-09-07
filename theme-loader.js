d68d50d72cf9a8e5f93cf82bd5c5681fc7512e7b
Aezoru <the4crazy4@gmail.com>
2025-09-07 20:37:59 +0200

    Delete theme-loader.js

--- theme-loader.js
+++
@@ -1,7 +0,0 @@
-// js/theme-loader.js
-(function() {
-    const savedTheme = localStorage.getItem('lunaris-theme') || 'dark';
-    if (savedTheme === 'light') {
-        document.body.classList.add('light-theme');
-    }
-})();
